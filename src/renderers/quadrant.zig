314462f2b612b7b53645d8cf0f3caeae4e200e53
TsunamiNoAi <spam@falseblue.com>
2026-04-15 13:24:28 -0400

    feat(renderers): allow customizable font family for SVG text elements. Closes #7

--- src/renderers/quadrant.zig
+++ src/renderers/quadrant.zig
@@ -79,15 +79,15 @@ pub fn render(allocator: std.mem.Allocator, value: Value) ![]const u8 {
         const yl_top_y = plot_y + 4;
         const frag_bot = try std.fmt.bufPrint(&buf,
             "<text x=\"{d:.1}\" y=\"{d:.1}\" fill=\"{s}\" font-size=\"{d}\" text-anchor=\"end\" " ++
-            "font-family=\"trebuchet ms,verdana,arial,sans-serif\" " ++
+            "font-family=\"{s}\" " ++
             "transform=\"rotate(-90 {d:.1} {d:.1})\">{s}</text>\n",
-            .{ yl_x, yl_bot_y, theme.text_color, qfs, yl_x, yl_bot_y, y_bottom });
+            .{ yl_x, yl_bot_y, theme.text_color, qfs, theme.font_family, yl_x, yl_bot_y, y_bottom });
         try svg.raw(frag_bot);
         const frag_top = try std.fmt.bufPrint(&buf,
             "<text x=\"{d:.1}\" y=\"{d:.1}\" fill=\"{s}\" font-size=\"{d}\" text-anchor=\"end\" " ++
-            "font-family=\"trebuchet ms,verdana,arial,sans-serif\" " ++
+            "font-family=\"{s}\" " ++
             "transform=\"rotate(-90 {d:.1} {d:.1})\">{s}</text>\n",
-            .{ yl_x, yl_top_y, theme.text_color, qfs, yl_x, yl_top_y, y_top });
+            .{ yl_x, yl_top_y, theme.text_color, qfs, theme.font_family, yl_x, yl_top_y, y_top });
         try svg.raw(frag_top);
     }
 
